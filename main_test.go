a30dad13de1941f4899bf86035da40ba6cdd46df
muzakkir <mohd.saifi@nashtechglobal.com>
2023-08-31 17:36:32 +0530

    Few updates

--- main_test.go
+++ main_test.go
@@ -1,311 +1,277 @@
 package main
 
-
 // Import key modules for this code ...
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
 
-    "encoding/json"
-    "fmt"
-    "log"
-    "os"
-    "testing"
-    "io/ioutil"
-    "net/http"
-    "os/exec"
-    "strings"
-    "github.com/gruntwork-io/terratest/modules/terraform"
-    "github.com/stretchr/testify/assert"
-
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 // define the variables
 var (
+	globalBackendConf = make(map[string]interface{})
 
-    globalBackendConf = make(map[string]interface{})
+	globalEnvVars = make(map[string]string)
 
-    globalEnvVars     = make(map[string]string)
-
-    subscription_name = "test-subscription"
-
+	subscription_name = "test-subscription"
 )
 
 // define the structure for function
 
 type Subscription struct {
+	ID string `json:"id"`
 
-    ID          string `json:"id"`
+	DisplayName string `json:"displayName"`
 
-    DisplayName string `json:"displayName"`
+	State string `json:"state"`
 
-    State       string `json:"state"`
-
-    TenantID    string `json:"tenantId"`
-
+	TenantID string `json:"tenantId"`
 }
-
 
 // define the const variable
 const (
-
-    apiVersion              = "2020-09-01"
-
+	apiVersion = "2020-09-01"
 )
 
-// fucntion to setup the credentials 
+// fucntion to setup the credentials
 
 func setTerraformVariables() (map[string]string, error) {
 
-    // Getting enVars from environment variables
+	// Getting enVars from environment variables
 
-    ARM_CLIENT_ID := os.Getenv("AZURE_CLIENT_ID")
+	CLIENT_ID := os.Getenv("AZURE_CLIENT_ID")
 
-    ARM_CLIENT_SECRET := os.Getenv("AZURE_CLIENT_SECRET")
+	CLIENT_SECRET := os.Getenv("AZURE_CLIENT_SECRET")
 
-    ARM_TENANT_ID := os.Getenv("AZURE_TENANT_ID")
+	TENANT_ID := os.Getenv("AZURE_TENANT_ID")
 
-    ARM_SUBSCRIPTION_ID := os.Getenv("AZURE_SUBSCRIPTION_ID")
+	SUBSCRIPTION_ID := os.Getenv("AZURE_SUBSCRIPTION_ID")
 
+	// Creating globalEnVars for terraform call through Terratest
 
-    // Creating globalEnVars for terraform call through Terratest
+	if CLIENT_ID != "" {
 
-    if ARM_CLIENT_ID != "" {
+		globalEnvVars["CLIENT_ID"] = CLIENT_ID
 
-        globalEnvVars["ARM_CLIENT_ID"] = ARM_CLIENT_ID
+		globalEnvVars["CLIENT_SECRET"] = CLIENT_SECRET
 
-        globalEnvVars["ARM_CLIENT_SECRET"] = ARM_CLIENT_SECRET
+		globalEnvVars["SUBSCRIPTION_ID"] = SUBSCRIPTION_ID
 
-        globalEnvVars["ARM_SUBSCRIPTION_ID"] = ARM_SUBSCRIPTION_ID
+		globalEnvVars["TENANT_ID"] = TENANT_ID
 
-        globalEnvVars["ARM_TENANT_ID"] = ARM_TENANT_ID
+	}
 
-    }
-
-
-    return globalEnvVars, nil
+	return globalEnvVars, nil
 
 }
-
 
 // function to  run the test cases
 
 func TestTerraform_azure_subscription(t *testing.T) {
 
-    t.Parallel()
+	t.Parallel()
 
-    setTerraformVariables()
+	setTerraformVariables()
 
+	// Use Terratest to deploy the infrastructure
 
-    // Use Terratest to deploy the infrastructure
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 
-    terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
 
-        // Set the path to the Terraform code that will be tested.
+		TerraformDir: "../module",
 
-        TerraformDir: "../module",
+		// Variables to pass to our Terraform code using -var options
 
+		Vars: map[string]interface{}{
 
-        // Variables to pass to our Terraform code using -var options
+			"subscription_name": subscription_name,
+		},
 
-        Vars: map[string]interface{}{
+		// globalvariables for user account
 
-            "subscription_name": subscription_name,
+		EnvVars: globalEnvVars,
 
-        },
+		// Backend values to set when initialziing Terraform
 
-        // globalvariables for user account
+		BackendConfig: globalBackendConf,
 
-        EnvVars: globalEnvVars,
+		// Disable colors in Terraform commands so its easier to parse stdout/stderr
 
-        // Backend values to set when initialziing Terraform
+		NoColor: true,
 
-        BackendConfig: globalBackendConf,
+		// Reconfigure is required if module deployment and go test pipelines are running in one stage
 
-        // Disable colors in Terraform commands so its easier to parse stdout/stderr
+		Reconfigure: true,
+	})
 
-        NoColor: true,
+	// terraform destroy will run in the last
+	defer terraform.Destroy(t, terraformOptions)
 
-        // Reconfigure is required if module deployment and go test pipelines are running in one stage
+	// this will run the terraform init and terraform apply
+	terraform.InitAndApply(t, terraformOptions)
 
-        Reconfigure: true,
+	// this will run the terraform init and terraform plan
 
-    })
+	// terraform.InitAndPlan(t, terraformOptions)
 
-// terraform destroy will run in the last
-    defer terraform.Destroy(t, terraformOptions)
+	expectedsubscription_id := terraform.Output(t, terraformOptions, "subscription_id")
 
+	expectedsubscription_name := terraform.Output(t, terraformOptions, "subscription_name")
 
-// this will run the terraform init and terraform apply 
-    terraform.InitAndApply(t, terraformOptions)
+	expectedazuerm_subscription_tenant_id := terraform.Output(t, terraformOptions, "azuerm_subscription_tenant_id")
 
-// this will run the terraform init and terraform plan 
+	fmt.Println("PRINTING THE RESOURCE PROPERTIES FROM OUTPUT FILE......................................")
 
-    // terraform.InitAndPlan(t, terraformOptions)
+	fmt.Printf("subscription_id : %s\n", expectedsubscription_id)
 
+	fmt.Printf("subscription_name : %s\n", expectedsubscription_name)
 
-    expectedsubscription_id := terraform.Output(t, terraformOptions, "subscription_id")
+	fmt.Printf("azuerm_subscription_tenant_id : %s\n", expectedazuerm_subscription_tenant_id)
 
-    expectedsubscription_name := terraform.Output(t, terraformOptions, "subscription_name")
+	fmt.Println("PRINTING THE RESOURCE PROPERTIES FROM OUTPUT FILE HAS BEEN ENDED........................")
 
-    expectedazuerm_subscription_tenant_id := terraform.Output(t, terraformOptions, "azuerm_subscription_tenant_id")
+	accessToken, err := getAccessToken()
 
+	if err != nil {
 
-    fmt.Println("PRINTING THE RESOURCE PROPERTIES FROM OUTPUT FILE......................................")
+		fmt.Printf("Failed to get access token: %s\n", err.Error())
 
-    fmt.Printf("subscription_id : %s\n", expectedsubscription_id)
+		return
 
-    fmt.Printf("subscription_name : %s\n", expectedsubscription_name)
+	}
 
-    fmt.Printf("azuerm_subscription_tenant_id : %s\n", expectedazuerm_subscription_tenant_id)
+	url := fmt.Sprintf("https://management.azure.com/subscriptions/%s?api-version=%s", expectedsubscription_id, apiVersion)
 
+	fmt.Println("Here the URL for the subscription Module Rest API:", url)
 
-    fmt.Println("PRINTING THE RESOURCE PROPERTIES FROM OUTPUT FILE HAS BEEN ENDED........................")
+	JSON, err := SubscriptionInfo(url, accessToken)
 
+	if err != nil {
 
-    accessToken, err := getAccessToken()
+		fmt.Printf("Failed to get subscription details: %s\n", err.Error())
 
-    if err != nil {
+		return
 
-        fmt.Printf("Failed to get access token: %s\n", err.Error())
+	}
 
-        return
+	actual_data, err := fetchSubscriptionInfo(JSON)
 
-    }
+	if err != nil {
 
-    url := fmt.Sprintf("https://management.azure.com/subscriptions/%s?api-version=%s", expectedsubscription_id, apiVersion)
+		log.Fatalf("failed to obtain a tha values from the RESTAPI: %v", err)
 
-    fmt.Println("Here the URL for the subscription Module Rest API:", url)
+	}
 
-   
+	fmt.Println("\nSubscription Details from REST API - opened :-----------------")
 
+	fmt.Printf("Display Name: %s\n", actual_data.DisplayName)
+	fmt.Printf("Tenant ID: %s\n", actual_data.TenantID)
 
+	fmt.Println("\nSubscription Details from REST API - closed :-----------------")
 
+	// Test cases
 
-    JSON, err := SubscriptionInfo(url, accessToken)
+	fmt.Println("Test cases are  running........")
 
-    if err != nil {
+	t.Run("Subscription_Name has been matched..", func(t *testing.T) {
 
-        fmt.Printf("Failed to get subscription details: %s\n", err.Error())
+		assert.Equal(t, expectedsubscription_name, actual_data.DisplayName)
 
-        return
+	})
 
-    }
+	t.Run("Tenant_Id has been matched..", func(t *testing.T) {
 
-    actual_data, err := fetchSubscriptionInfo(JSON)
+		assert.Equal(t, expectedazuerm_subscription_tenant_id, actual_data.TenantID)
 
-    if err != nil {
-
-        log.Fatalf("failed to obtain a tha values from the RESTAPI: %v", err)
-
-    }
-
-    fmt.Println("\nSubscription Details from REST API - opened :-----------------")
-
-    fmt.Printf("Display Name: %s\n", actual_data.DisplayName)
-    fmt.Printf("Tenant ID: %s\n", actual_data.TenantID)
-
-    fmt.Println("\nSubscription Details from REST API - closed :-----------------")
-
-    // Test cases 
-
-    fmt.Println("Test cases are  running........")
-
-    t.Run("Subscription_Name has been matched..", func(t *testing.T) {
-
-        assert.Equal(t, expectedsubscription_name, actual_data.DisplayName)
-
-    })
-
-    t.Run("Tenant_Id has been matched..", func(t *testing.T) {
-
-        assert.Equal(t, expectedazuerm_subscription_tenant_id, actual_data.TenantID)
-
-    })
-
-
-
+	})
 
 }
 
-
 func getAccessToken() (string, error) {
 
-    cmd := exec.Command("az", "account", "get-access-token", "--query", "accessToken", "--output", "tsv")
+	cmd := exec.Command("az", "account", "get-access-token", "--query", "accessToken", "--output", "tsv")
 
-    output, err := cmd.Output()
+	output, err := cmd.Output()
 
-    if err != nil {
+	if err != nil {
 
-        return "", err
+		return "", err
 
-    }
+	}
 
-    return strings.TrimSpace(string(output)), nil
+	return strings.TrimSpace(string(output)), nil
 
 }
 
 func SubscriptionInfo(url, accessToken string) ([]byte, error) {
 
-    client := &http.Client{}
+	client := &http.Client{}
 
-    req, err := http.NewRequest(http.MethodGet, url, nil)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 
-    if err != nil {
+	if err != nil {
 
-        return nil, err
+		return nil, err
 
-    }
+	}
 
-    req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
 
-    resp, err := client.Do(req)
+	resp, err := client.Do(req)
 
-    if err != nil {
+	if err != nil {
 
-        return nil, err
+		return nil, err
 
-    }
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 
-    if err != nil {
+	if err != nil {
 
-        return nil, err
+		return nil, err
 
-    }
+	}
 
-    return body, nil
+	return body, nil
 
 }
 
-
 func fetchSubscriptionInfo(subscriptionJSON []byte) (Subscription, error) {
 
-    fmt.Println("#############################---Complete JSON Response from the RESTAPI---#######################################")
+	fmt.Println("#############################---Complete JSON Response from the RESTAPI---#######################################")
 
-    fmt.Println(string(subscriptionJSON))
+	fmt.Println(string(subscriptionJSON))
 
-    fmt.Println("#############################---Complete JSON Response from the RESTAPI---#######################################")
+	fmt.Println("#############################---Complete JSON Response from the RESTAPI---#######################################")
 
-    var subscription Subscription
+	var subscription Subscription
 
-    // unmarshal is use to convert the json ot xml data into go data structure
+	// unmarshal is use to convert the json ot xml data into go data structure
 
-    err := json.Unmarshal(subscriptionJSON, &subscription)
+	err := json.Unmarshal(subscriptionJSON, &subscription)
 
-    if err != nil {
+	if err != nil {
 
-        fmt.Printf("Failed to unmarshal JSON response: %s\n", err.Error())
+		fmt.Printf("Failed to unmarshal JSON response: %s\n", err.Error())
 
-        return Subscription{}, err
+		return Subscription{}, err
 
-    }
+	}
 
-    return subscription, err
-
-
-
+	return subscription, err
 
 }
